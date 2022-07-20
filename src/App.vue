054ec53999cd0d9739dbea12e0f80b1b46aad3d4
Naruto <moyuanzheng@gmail.com>
2022-07-20 13:58:09 +0800

    three与vue对接

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 
 <template>
-1111122222
+  <div class="three-canvas"
+       ref="threeTarget"></div>
 </template>
 
-
-
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import { TEngine } from './assets/ts/TEngine'
 
 export default defineComponent({
   setup() {
-    
+    const threeTarget = ref(null)
+    onMounted(() => {
+      const TE = new TEngine(threeTarget.value)
+    })
+
+    return {
+      threeTarget,
+    }
   },
 })
 </script>
 <style>
-#app{
-  
+.three-canvas {
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
 }
 </style>
 
