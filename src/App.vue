86ee639b93a60ec55249de65a85a572dd5b4a34d
Naruto <moyuanzheng@gmail.com>
2022-07-20 11:08:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+1111122222
+</template>
+
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+<style>
+#app{
+  
+}
+</style>
+
+
